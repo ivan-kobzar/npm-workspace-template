cb5440db5545b3fb8104efcce3809b0d01c2edd1
Ivan Kobzar <ivan.kobzar@kilo.health>
2024-09-02 11:37:59 +0300

    feat: add hono rpc client

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,18 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const routes = app.get('/', (c) => {
+  return c.text('Hello Hono!');
+});
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+export type AppType = typeof routes;
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
